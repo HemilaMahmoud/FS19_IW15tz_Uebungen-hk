cac2812b59f37cad649e30da34557c453306ed06
Mahmoud Hemila <mahmoud@hemila.ch>
2019-02-19 18:53:04 +0100

    zählen von klickern

--- 02_javascript_clientside/02_starterpackage/js/klickzaehler.js
+++ 02_javascript_clientside/02_starterpackage/js/klickzaehler.js
@@ -1,3 +1,8 @@
 function klickZaehler() {
-  console.log("Hallo Velo");
+  //console.log("Hallo Velo");
+
+  var clicker = parseInt(document.getElementById("klicknumbers").innerHTML);
+  clickerAddition = clicker + 1;
+  output = document.getElementById("klicknumbers").innerHTML = clickerAddition;
+  return output;
 }
