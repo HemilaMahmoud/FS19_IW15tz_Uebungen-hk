0825fa8a692de8ea3d883b90057598077a9e7df8
Mahmoud Hemila <mahmoud@hemila.ch>
2019-02-27 11:36:37 +0100

    localStorage hinzugefügt

--- 02_javascript_clientside/02_starterpackage/js/klickzaehler.js
+++ 02_javascript_clientside/02_starterpackage/js/klickzaehler.js
@@ -1,8 +1,25 @@
-function klickZaehler() {
-  //console.log("Hallo Velo");
 
-  var clicker = parseInt(document.getElementById("klicknumbers").innerHTML);
-  clickerAddition = clicker + 1;
-  output = document.getElementById("klicknumbers").innerHTML = clickerAddition;
-  return output;
+window.onload = start;
+function klickZaehler() {
+
+  if(typeof(Storage)!==undefined){
+    if(localStorage.clickcount){
+      localStorage.clickcount = Number (localStorage.clickcount) + 1;
+    }
+    else{
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("klicknumbers").innerHTML = localStorage.clickcount;
+  //  document.getElementById("clickers").innerHTML = "Sie haben " + localStorage.clickcount + " geklickt";
+  }
+
+}
+
+function start (){
+  if(typeof(Storage)!==undefined){
+    if(localStorage.clickcount){
+      localStorage.clickcount = Number (localStorage.clickcount);
+    }
+    document.getElementById("klicknumbers").innerHTML = localStorage.clickcount;
+  }
 }
